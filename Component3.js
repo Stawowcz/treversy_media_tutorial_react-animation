f275ecc0aa4bb7bfcd140de795f82fb349c2549d
Stawowcz <52681098+Stawowcz@users.noreply.github.com>
2020-11-08 23:41:37 +0100

    Add files via upload

---
+++ Component3.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Component3() {
+    return (
+        <div style={c3Style}>
+            <h1>Component 3</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus
+            nobis adipisci eum minima deserunt at porro, veritatis officia
+            commodi itaque voluptates vel suscipit assumenda soluta ipsa
+            voluptatibus laudantium labore harum?</p>
+        </div>
+    )
+}
+
+const c3Style = {
+    background: 'slateblue',
+    color: 'white',
+    padding: '1.5rem'
+}
